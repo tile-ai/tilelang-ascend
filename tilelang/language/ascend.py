686b7a46d634cc29e4531ac1e57056c7f0b1e8b2
BiancaTong <tongbingxin@huawei.com>
2025-11-13 15:19:46 +0800

    debug float16

--- tilelang/language/ascend.py
+++ tilelang/language/ascend.py
@@ -1,6 +1,7 @@
 import tilelang.language as T
 from tvm.tir import PrimExpr, Buffer, BufferRegion, BufferLoad, Var
 from typing import List, Union, Literal
+import numpy as np
 
 import math
 
@@ -587,13 +588,13 @@ def compare(dst: Buffer, src0: Buffer, src1: Union[Buffer, BufferLoad, PrimExpr]
 def sync_all():
     return T.call_extern("handle", f"AscendC::SyncAll<false>")
 
-def cast_tl(dst: Buffer, src: Buffer, mode: str, count: PrimExpr, scale: Union[float16, float]):
+def cast_tl(dst: Buffer, src: Buffer, mode: str, count: PrimExpr, scale: PrimExpr):
     assert mode in ["CAST_NONE", "CAST_RINT", "CAST_FLOOR", "CAST_CEIL", "CAST_ROUND", "CAST_TRUNC", "CAST_ODD"]
 
     round_mode = f"AscendC::RoundMode::{mode}"
 
     # int32转half的场景，roundMode不生效，与SetDeqScale(half scale)接口配合使用
-    if (isinstance(src, int) and src >= 0 and src <= 0xFFFFFFFF) and isinstance(dst, float16):
+    if (isinstance(src, int) and src >= 0 and src <= 0xFFFFFFFF) and isinstance(dst, np.float16):
         T.call_extern("handle", f"AscendC::SetDeqScale", scale)
 
     return T.call_extern("handle", f"tl::ascend::cast<{_dtype(dst)}, {_dtype(src)}, {round_mode}, {count}>", dst.access_ptr("w"), src.access_ptr("r"))
