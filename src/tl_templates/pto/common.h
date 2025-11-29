3d8e261277b55dcfb03fabb793db3e46f1e3e93e
hanxu88 <hanxu88@huawei.com>
2025-11-29 12:06:23 +0800

    [Feat] Update pto common.h and  gemm_v0

--- src/tl_templates/pto/common.h
+++ src/tl_templates/pto/common.h
@@ -5,19 +5,28 @@
 namespace tl::pto {
 using namespace pto;
 
+template <typename T, int Rows, int Cols,
+          int RowValid = Rows, int ColValid = Cols>
+using TileMatL1 = Tile<Location::Mat, T, Rows, Cols,
+                       BLayout::ColMajor,
+                       RowValid, ColValid,
+                       SLayout::RowMajor,
+                       512>;
+
 template <typename T1, typename T2, uint32_t M, uint32_t N, uint32_t K,
           bool transpose_A = false, bool transpose_B = false>
-void gemm_v0(Tile<Location::Mat, T1, M, K, M, K, 512> const &A, // l1a
-             Tile<Location::Mat, T1, K, N, K, N, 512> const &B, // l1b
-             Tile<Location::Acc, T2, M, N, M, N, 512> const &C, // l0c
-             bool clear) {
+__aicore__ PTO_INLINE void gemm_v0(
+            TileMatL1<T1, M, K> const &A, // l1a
+            TileMatL1<T1, K, N> const &B, // l1b
+            TileAcc<T2, M, N> const &C,   // l0c
+            bool clear) {
     // Allocate l0a/l0b
-    Tile<Location::Left, T1, M, K, M, K> l0a_;
-    Tile<Location::Right, T1, K, N, K, N> l0b_;
+    TileLeft<T1, M, K> l0a;
+    TileRight<T1, K, N> l0b;
 
     // copy l1 to l0a/l0b
-    TEXTRACT(l0a_, A, 0, 0);
-    TEXTRACT(l0b_, B, 0, 0);
+    TEXTRACT(l0a, A, 0, 0);
+    TEXTRACT(l0b, B, 0, 0);
      
     if(clear) {
         TMATMUL(C, l0a, l0b);
