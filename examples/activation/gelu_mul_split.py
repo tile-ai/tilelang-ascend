33db7691ab7991c2c4c635f16fdf0c6859a03a0b
z00520135 <zhangwei503@huawei.com>
2025-12-15 20:35:42 +0800

    增加日志打印

--- examples/activation/gelu_mul_split.py
+++ examples/activation/gelu_mul_split.py
@@ -28,7 +28,7 @@ def gelu_mul(M, N, block_M, block_N, dtype="float"):
             with T.Scope("V"):
                 T.copy(A[bx * block_M + vid * block_M // VEC_NUM, by * block_N], a1_ub)
                 T.barrier_all()
-                T.copy(A[bx * block_M + vid * block_M // VEC_NUM + N // 2, by * block_N], a2_ub)
+                T.copy(A[bx * block_M + vid * block_M // VEC_NUM, by * block_N + N // 2], a2_ub)
                 T.barrier_all()
                 T.tile.mul(temp_ub, a1_ub, a1_ub)
                 T.barrier_all()
