afa87cd84e9a385010c5cc6b7d863b21cb1873e3
z00520135 <zhangwei503@huawei.com>
2025-12-16 17:56:06 +0800

    格式整改

--- examples/activation/gelu_mul_split.py
+++ examples/activation/gelu_mul_split.py
@@ -8,9 +8,11 @@ tilelang.cache.clear_cache()
 @tilelang.jit(out_idx=[1])
 def gelu_mul(M, N, block_M, block_N, dtype="float"):
     m_num = T.ceildiv(M, block_M)
+    # 算子将输入Tensor按照最后一个维度分为左右两个Tensor：x1和x2，对x1进行GELU计算，结果与x2相乘，因此分核只相对于x1的维度
     n_num = T.ceildiv(N // 2, block_N)
     
     VEC_NUM = 2
+
 
     @T.prim_func
     def main(
@@ -20,41 +22,43 @@ def gelu_mul(M, N, block_M, block_N, dtype="float"):
         with T.Kernel(m_num * n_num, is_npu=True) as (cid, vid):
             bx = cid // n_num
             by = cid % n_num
-            T.printf("-----cid:%d-------------vid:%d--------------------------------------\n", cid, vid)
-            T.printf("-----m_num:%d-------------n_num:%d--------------------------------------\n", m_num, n_num)
             a1_ub = T.alloc_ub((block_M // VEC_NUM, block_N), dtype)
             a2_ub = T.alloc_ub((block_M // VEC_NUM, block_N), dtype)
             b_ub = T.alloc_ub((block_M // VEC_NUM, block_N), dtype)
             temp_ub = T.alloc_ub((block_M // VEC_NUM, block_N), dtype)
             with T.Scope("V"):
-                T.printf("inner-----cid:%d-------------vid:%d--------------------------------------\n", cid, vid)
+                # 左半部分用a1_ub缓存
                 T.copy(A[bx * block_M + vid * block_M // VEC_NUM, by * block_N], a1_ub)
                 T.barrier_all()
-                # T.printf("===========a1_ub before copy:\n")
-                # T.dump_tensor(a1_ub, 222, block_M // VEC_NUM * block_N, (block_M // VEC_NUM, block_N))
-                # T.barrier_all()
+                # 右半部分用a2_ub缓存
                 T.copy(A[bx * block_M + vid * block_M // VEC_NUM, by * block_N + N // 2], a2_ub)
                 T.barrier_all()
+                # 计算公式：x^2
                 T.tile.mul(temp_ub, a1_ub, a1_ub)
                 T.barrier_all()
+                # 计算公式：x^3
                 T.tile.mul(temp_ub, a1_ub, temp_ub)
                 T.barrier_all()
+                # 计算公式：0.044715 * x^3
                 T.tile.mul(temp_ub, temp_ub, 0.044715)
                 T.barrier_all()
+                # 计算公式：x + 0.044715 * x^3
                 T.tile.add(temp_ub, a1_ub, temp_ub)
                 T.barrier_all()
+                # 计算公式：-sqrt(8/pi)(x + 0.044715 * x^3)
                 T.tile.mul(temp_ub, temp_ub, -1.5957691)
                 T.barrier_all()
+                # 计算公式：exp(-sqrt(8/pi)(x + 0.044715 * x^3))
                 T.tile.exp(temp_ub, temp_ub)
                 T.barrier_all()
+                # 计算公式：1 + exp(-sqrt(8/pi)(x + 0.044715 * x^3))
                 T.tile.add(temp_ub, temp_ub, 1.0)
                 T.barrier_all()
-                T.tile.div(b_ub, a1_ub, temp_ub)
+                # 计算公式：x / (1 + exp(-sqrt(8/pi)(x + 0.044715 * x^3)))
+                T.tile.div(temp_ub, a1_ub, temp_ub)
                 T.barrier_all()
-                # T.printf("===========b_ub after copy:\n")
-                # T.dump_tensor(b_ub, 222, block_M // VEC_NUM * block_N, (block_M // VEC_NUM, block_N))
-                # T.barrier_all()
-                T.tile.mul(b_ub, b_ub, a2_ub)
+                # 左半部分计算结果与右半部分相乘
+                T.tile.mul(b_ub, temp_ub, a2_ub)
                 T.barrier_all()
                 T.copy(b_ub, B[bx * block_M + vid * block_M // VEC_NUM, by * block_N])
 
@@ -62,11 +66,10 @@ def gelu_mul(M, N, block_M, block_N, dtype="float"):
 
 
 torch.manual_seed(0)
-# Tests
+# 测试数据
 test_configs = [
     (256, 256, 64, 64),
     (1024, 1024, 128, 128),
-    # (1100, 50000, 128, 128),
 ]
 
 for M, N, block_M, block_N in test_configs:
@@ -78,9 +81,7 @@ for M, N, block_M, block_N in test_configs:
     print(func.get_kernel_source())
     gelu = nn.GELU(approximate='tanh')
     a1, a2 = torch.split(a, N // 2, dim=1)
-    # ref_b = gelu(a1)
     ref_b = gelu(a1) * a2
-    # print("ref_b", ref_b)
     torch.testing.assert_close(b.cpu(), ref_b.cpu(), rtol=1e-2, atol=1e-2)
     print("Test passed!")
 
