9386432b5a98bd943d8c6ce90284ed7e5e4cee7d
z00520135 <zhangwei503@huawei.com>
2025-12-19 17:16:01 +0800

    多维度分割支持

--- examples/activation/swi_glu.py
+++ examples/activation/swi_glu.py
@@ -5,14 +5,15 @@ import torch.nn as nn
 
 tilelang.cache.clear_cache()
 
-# pass_configs = {
-#     tilelang.PassConfigKey.TL_ASCEND_AUTO_SYNC: True,
-#     tilelang.PassConfigKey.TIR_MERGE_STATIC_SMEM: True,
-#     tilelang.PassConfigKey.TL_ASCEND_AUTO_CV_COMBINE: True,
-# }
+pass_configs = {
+    tilelang.PassConfigKey.TL_ASCEND_AUTO_SYNC: True,
+    tilelang.PassConfigKey.TL_ASCEND_AUTO_CV_COMBINE: True,
+}
 
-@tilelang.jit(out_idx=[1])
+@tilelang.jit(out_idx=[1], pass_configs=pass_configs)
 def swi_glu(M, N, block_M, block_N, split_dim, dtype="float"):
+    # The `swi_glu` operator splits the input tensor into two tensors, x1 and x2, based on the split dimension. 
+    # It performs a Swish operation on x1 and multiplies the result by x2. 
     m_div = 1
     n_div = 2
     m_offset = 0
@@ -22,10 +23,7 @@ def swi_glu(M, N, block_M, block_N, split_dim, dtype="float"):
         n_div = 1
         m_offset = M // 2
         n_offset = 0
-
     m_num = T.ceildiv(M // m_div, block_M)
-    # The `swi_glu` operator splits the input tensor into two tensors, x1 and x2, based on the last dimension. 
-    # It performs a GELU operation on x1 and multiplies the result by x2. Therefore, the kernel splitting is only relative to the dimension of x1.
     n_num = T.ceildiv(N // n_div, block_N)
     
     VEC_NUM = 2
@@ -45,24 +43,23 @@ def swi_glu(M, N, block_M, block_N, split_dim, dtype="float"):
             zero_ub = T.alloc_ub((block_M // VEC_NUM, block_N), dtype)
             temp_ub = T.alloc_ub((block_M // VEC_NUM, block_N), dtype)
 
-            with T.Scope("V"):
-                T.copy(A[bx * block_M + vid * block_M // VEC_NUM, by * block_N], a0_ub)
-                T.barrier_all()
-                T.copy(A[bx * block_M + vid * block_M // VEC_NUM + m_offset, by * block_N + n_offset], a1_ub)
-                T.barrier_all()
-                T.tile.fill(zero_ub, 0.0)
-                T.barrier_all()
-                T.tile.sub(temp_ub, zero_ub, a0_ub)
-                T.barrier_all()
-                T.tile.exp(temp_ub, temp_ub)
-                T.barrier_all()
-                T.tile.add(temp_ub, temp_ub, 1.0)
-                T.barrier_all()
-                T.tile.div(temp_ub, a0_ub, temp_ub)
-                T.barrier_all()
-                T.tile.mul(b_ub, temp_ub, a1_ub)
-                T.barrier_all()
-                T.copy(b_ub, B[bx * block_M + vid * block_M // VEC_NUM, by * block_N])
+            ## [In vector]
+            # The first half is cached using a0_ub
+            T.copy(A[bx * block_M + vid * block_M // VEC_NUM, by * block_N], a0_ub)
+            # The second half is cached using a1_ub
+            T.copy(A[bx * block_M + vid * block_M // VEC_NUM + m_offset, by * block_N + n_offset], a1_ub)
+            T.tile.fill(zero_ub, 0.0)
+            # Calculation formula:-x
+            T.tile.sub(temp_ub, zero_ub, a0_ub)
+            # Calculation formula:exp(-x)
+            T.tile.exp(temp_ub, temp_ub)
+            # Calculation formula:1 + exp(-x)
+            T.tile.add(temp_ub, temp_ub, 1.0)
+            # Calculation formula:x / (1 + exp(-x))
+            T.tile.div(temp_ub, a0_ub, temp_ub)
+            # Multiply the result of the first half by the second half
+            T.tile.mul(b_ub, temp_ub, a1_ub)
+            T.copy(b_ub, B[bx * block_M + vid * block_M // VEC_NUM, by * block_N])
 
     return main
 
@@ -77,7 +74,7 @@ test_configs = [
 ]
 
 for M, N, block_M, block_N, split_dim in test_configs:
-    print(f"Testing swi_gul with M={M}, N={N}, block_M={block_M}, block_N={block_N}")
+    print(f"Testing swi_gul with M={M}, N={N}, block_M={block_M}, block_N={block_N}, split_dim={split_dim}")
     func = swi_glu(M, N, block_M, block_N, split_dim)
     print("Init successful!")
     a = torch.randn(M, N, dtype=torch.float).npu()
