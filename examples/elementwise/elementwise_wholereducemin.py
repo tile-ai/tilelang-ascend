0bced26601e10915f222c27db9c9bf1e83c49c39
hedi515 <hedi7@huawei.com>
2025-12-30 21:12:12 +0800

    [Add] add bilinearinterpolation&wholereducemax/min/sum

---
+++ examples/elementwise/elementwise_wholereducemin.py
@@ -0,0 +1,81 @@
+import argparse
+
+import tilelang
+import tilelang.language as T
+import torch
+
+tilelang.cache.clear_cache()
+
+parser = argparse.ArgumentParser(description="NPU Kernel Compilation")
+parser.add_argument("--m", type=int, default=1024, help="Matrix M dimension")
+parser.add_argument("--n", type=int, default=1024, help="Matrix N dimension")
+args = parser.parse_args()
+
+M = 2
+N = 512
+block_M = 2
+block_N = 128
+mask = 64
+repeatTimes = 2
+dstRepStride = 1
+srcBlkStride = 1
+srcRepStride = 4
+
+pass_configs = {
+    tilelang.PassConfigKey.TL_ASCEND_AUTO_SYNC: True,
+    tilelang.PassConfigKey.TL_ASCEND_AUTO_CV_COMBINE: True,
+}
+@tilelang.jit(out_idx=[-1], pass_configs=pass_configs)
+def wholereducemin(M, N, block_M, block_N, mask, repeatTimes, dstRepStride, srcBlkStride, srcRepStride, dtype="float16"):
+    m_num = M // block_M
+    n_num = N // block_N
+
+    VEC_NUM = 2
+
+    @T.prim_func
+    def main(
+            A: T.Tensor((M, N), dtype),
+            B: T.Tensor((M, 2 * N // mask), dtype),
+    ):
+        with T.Kernel(m_num * n_num, is_npu=True) as (cid, vid):
+            bx = cid // n_num
+            by = cid % n_num
+
+            a_ub = T.alloc_ub((block_M // VEC_NUM, block_N), dtype)
+            b_ub = T.alloc_ub((block_M // VEC_NUM, 2 * block_N // mask), dtype)
+
+            T.copy(A[bx * block_M + vid * block_M // VEC_NUM, by * block_N], a_ub)
+            T.tile.wholereducemin(b_ub, a_ub, mask, repeatTimes, dstRepStride, srcBlkStride, srcRepStride)
+            T.copy(b_ub, B[bx * block_M + vid * block_M // VEC_NUM, by * 2 * block_N // mask])
+
+    return main
+
+
+func = wholereducemin(M, N, block_M, block_N, mask, repeatTimes, dstRepStride, srcBlkStride, srcRepStride)
+
+torch.manual_seed(0)
+
+a = torch.randn(M, N, dtype=torch.float16).npu()
+
+torch.npu.synchronize()
+print("init successful!")
+
+b = func(a)
+
+num_groups = M * N // mask
+ref_b = torch.zeros((1, 2 * num_groups)).to(torch.float16)
+a_flag = a.reshape(-1)
+for i in range(num_groups):
+    start = i * mask
+    end = start + mask
+    group = a_flag[start:end]
+    min_val = torch.min(group).item()
+    min_idx_in_group = torch.argmin(group).item()
+    result = torch.tensor([min_idx_in_group], dtype=torch.uint16).view(torch.float16).float().item()
+    ref_b[0, 2 * i] = min_val
+    ref_b[0, 2 * i + 1] = result
+ref_b = ref_b.reshape(M, 2 * N // mask)
+ref_b = ref_b.npu().to(dtype=torch.float16)
+
+torch.testing.assert_close(b, ref_b, rtol=1e-2, atol=1e-2)
+print("Kernel Output Match!")
--- src/target/codegen_ascend.cc
+++ src/target/codegen_ascend.cc
@@ -749,6 +749,73 @@ void CodeGenTileLangAscend::VisitExpr_(const CallNode *op, std::ostream &os) {
         this->stream << ", " << PrintExpr(op->args[i]);
       }
       this->stream << ");\n";
+    } else if (op_name == "AscendC::BilinearInterpolation") {
+      this->PrintIndent();
+      auto var_name = print_buffer_offset(op->args[1].as<CallNode>());
+      auto var_name_1 = print_buffer_offset(op->args[2].as<CallNode>());
+      auto var_name_2 = print_buffer_offset(op->args[3].as<CallNode>());
+      auto var_name_3 = print_buffer_offset(op->args[4].as<CallNode>());
+      auto var_name_4 = print_buffer_offset(op->args[11].as<CallNode>());
+      this->stream << op_name << "(" << var_name << ", "
+                   << var_name_1 << ", " << var_name_2
+                   << ", " << var_name_3 << ", " << PrintExpr(op->args[5])
+                   << ", " << PrintExpr(op->args[6]) << ", " << PrintExpr(op->args[7])
+                   << ", " << PrintExpr(op->args[8]) << ", " << PrintExpr(op->args[9])
+                   << ", " << PrintExpr(op->args[10]) << ", " << var_name_4 << ");\n";
+    } else if (op_name == "AscendC::WholeReduceMax") {
+      std::vector<std::string> var_names;
+      for (int i = 1; i < 3; i++) {
+        auto var_name = print_buffer_offset(op->args[i].as<CallNode>());
+        var_names.push_back(var_name);
+      }
+      this->PrintIndent();
+      this->stream << op_name << "(";
+      for (int i = 0; i < var_names.size(); i++) {
+        this->stream << var_names[i];
+        if (i != var_names.size() - 1) {
+          this->stream << ", ";
+        }
+      }
+      for (int i = 3; i < op->args.size() - 1; i++) {
+        this->stream << ", " << PrintExpr(op->args[i]);
+      }
+      this->stream << ", " << "AscendC::ReduceOrder::" << Downcast<StringImm>(op->args[op->args.size() - 1])->value << ");\n";
+    } else if (op_name == "AscendC::WholeReduceMin") {
+      std::vector<std::string> var_names;
+      for (int i = 1; i < 3; i++) {
+        auto var_name = print_buffer_offset(op->args[i].as<CallNode>());
+        var_names.push_back(var_name);
+      }
+      this->PrintIndent();
+      this->stream << op_name << "(";
+      for (int i = 0; i < var_names.size(); i++) {
+        this->stream << var_names[i];
+        if (i != var_names.size() - 1) {
+          this->stream << ", ";
+        }
+      }
+      for (int i = 3; i < op->args.size() - 1; i++) {
+        this->stream << ", " << PrintExpr(op->args[i]);
+      }
+      this->stream << ", " << "AscendC::ReduceOrder::" << Downcast<StringImm>(op->args[op->args.size() - 1])->value << ");\n";
+    } else if (op_name == "AscendC::WholeReduceSum") {
+      std::vector<std::string> var_names;
+      for (int i = 1; i < 3; i++) {
+        auto var_name = print_buffer_offset(op->args[i].as<CallNode>());
+        var_names.push_back(var_name);
+      }
+      this->PrintIndent();
+      this->stream << op_name << "(";
+      for (int i = 0; i < var_names.size(); i++) {
+        this->stream << var_names[i];
+        if (i != var_names.size() - 1) {
+          this->stream << ", ";
+        }
+      }
+      for (int i = 3; i < op->args.size(); i++) {
+        this->stream << ", " << PrintExpr(op->args[i]);
+      }
+      this->stream << ");\n";
     } else if (op_name == "AscendC::Muls" || op_name == "AscendC::Adds") {
       std::vector<std::string> var_names;
       for (int i = 1; i < 3; i++) {
