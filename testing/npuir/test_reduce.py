089173b1228a4466737252d7a88a98f9f05e0f30
lzp1021 <96581909+lzp1021@users.noreply.github.com>
2026-01-08 14:24:12 +0800

    Update test_reduce.py

--- testing/npuir/test_reduce.py
+++ testing/npuir/test_reduce.py
@@ -2,6 +2,7 @@ import torch
 import argparse
 import tilelang
 import tilelang.language as T
+import os
 
 torch.npu.set_device(0)
 tilelang.cache.clear_cache()
@@ -14,32 +15,63 @@ parser.add_argument("--block_M", type=int, default=32, help="")
 dtype = "float16"
 accum_dtype = "float16"
 
-def row_reduce_sum(M, N, block_M):
+def row_reduce_sum_exp(M, N, block_M):
     BLOCK_SIZE = 1
 
     @T.prim_func
     def main(A: T.Tensor((M, N), dtype),
                 B: T.Tensor((M, N), dtype),
+                C: T.Tensor((M, N), dtype),
+                D: T.Tensor((M, N), dtype),
                 O: T.Tensor((M,1), accum_dtype)):
         
         with T.Kernel(BLOCK_SIZE, is_npu=True) as (cid, _):
+            a = T.alloc_ub((M,N), dtype)
+            b = T.alloc_ub((M,N), dtype)
+            c = T.alloc_ub((M,N), dtype)
+            d = T.alloc_ub((M,N), dtype)
+            s = T.alloc_ub((M,1), accum_dtype)
 
-            x = T.alloc_shared((M,N), dtype)
-            y = T.alloc_shared((M,N), dtype)
+            T.copy(A, a)
+            T.copy(B, b)
+            T.copy(C, c)
+            T.copy(D, d)
+            
+            T.reduce_abssum(a, s)
+            T.reduce_max(b, s, clear = False)
+            T.reduce_min(c, s, clear = False)
+            T.reduce(d, s, dims=1, reduce_mode="sum", clear = False)
+
+            T.copy(s, O)
+
+    return main
+
+def row_reduce_sum_dev(M, N, block_M):
+    BLOCK_SIZE = 1
+
+    @T.prim_func
+    def main(A: T.Tensor((M, N), dtype),
+                B: T.Tensor((M, N), dtype),
+                C: T.Tensor((M, N), dtype),
+                D: T.Tensor((M, N), dtype),
+                O: T.Tensor((M,1), accum_dtype)):
+        
+        with T.Kernel(BLOCK_SIZE, is_npu=True) as (cid, _):
+            a = T.alloc_shared((M,N), dtype)
+            b = T.alloc_shared((M,N), dtype)
+            c = T.alloc_shared((M,N), dtype)
+            d = T.alloc_shared((M,N), dtype)
             s = T.alloc_shared((M,1), accum_dtype)
 
-            T.copy(A, x)
-            T.copy(B, y)
-
-            # T.reduce_max(x, s)
-            # T.reduce(x, s, 1, "max")
-            # T.reduce_max(x, s)
-            # T.reduce_max(y, s, clear = False)
-            # T.reduce_min(x, s)
-            # T.reduce_sum(x, s, dim=1)
-            # T.reduce(y, s, dims=1, reduce_mode="sum", clear = False)
-            T.reduce(x, s, dims=1, reduce_mode="abssum")
-            T.reduce(y, s, dims=1, reduce_mode="abssum", clear = False)
+            T.copy(A, a)
+            T.copy(B, b)
+            T.copy(C, c)
+            T.copy(D, d)
+            
+            T.reduce_abssum(a, s)
+            T.reduce_max(b, s, clear = False)
+            T.reduce_min(c, s, clear = False)
+            T.reduce(d, s, dims=1, reduce_mode="sum", clear = False)
 
             T.copy(s, O)
 
@@ -60,11 +92,18 @@ def generate_tensor(shape, dtype, clear=False):
     raise ValueError('Invalid parameter "dtype" is found : {}'.format(dtype))
 
 def main(main_args):
-    func = row_reduce_sum(
-        main_args.M,
-        main_args.N,
-        main_args.block_M
-    )
+    if os.environ['TILELANG_ASCEND_MODE'] == 'Expert':
+        func = row_reduce_sum_exp(
+            main_args.M,
+            main_args.N,
+            main_args.block_M
+        )
+    else:
+        func = row_reduce_sum_dev(
+            main_args.M,
+            main_args.N,
+            main_args.block_M
+        )
 
     compiled_kernel = tilelang.compile(func, target='npuir')
 
@@ -74,20 +113,17 @@ def main(main_args):
     shape2 = (main_args.M,1)
     A = generate_tensor(shape, dtype).npu()
     B = generate_tensor(shape, dtype).npu()
+    C = generate_tensor(shape, dtype).npu()
+    D = generate_tensor(shape, dtype).npu()
     O = generate_tensor(shape2, accum_dtype, True).npu()
 
-    compiled_kernel(A, B, O)
-    # res1 = torch.max(A, dim=1, keepdim=True).values
-    # res2 = torch.max(B, dim=1, keepdim=True).values
-    # res = torch.maximum(res1, res2)
-    # res = torch.min(A, dim=1, keepdim=True).values
-    # res = torch.sum(A, dim=1, keepdim=True)
-    # res += torch.sum(B, dim=1, keepdim=True)
-    res = torch.sum(torch.abs(A), dim=1, keepdim=True)
-    res += torch.sum(torch.abs(B), dim=1, keepdim=True)
-    # res = torch.max(torch.abs(A), dim=1, keepdim=True).values
-    print(A)
-    print(B)
+    compiled_kernel(A, B, C, D, O)
+    res1 = torch.sum(torch.abs(A), dim=1, keepdim=True)
+    res2 = torch.max(B, dim=1, keepdim=True).values
+    res3 = torch.maximum(res1, res2)
+    res4 = torch.min(C, dim=1, keepdim=True).values
+    res5 = torch.minimum(res3, res4)
+    res = res5 + torch.sum(D, dim=1, keepdim=True)
     print("Actual Result:")
     print(O)
     print("Expected Result:")
@@ -98,4 +134,7 @@ def main(main_args):
 
 if __name__ == "__main__":
     args = parser.parse_args()
+    os.environ['TILELANG_ASCEND_MODE'] = 'Expert'
+    main(args)
+    os.environ['TILELANG_ASCEND_MODE'] = 'Developer'
     main(args)
