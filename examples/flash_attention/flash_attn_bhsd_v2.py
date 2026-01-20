71419eeb7305ccd0b19aa57b378f5c1786dcad08
liggest <liggest@sina.cn>
2026-01-20 17:22:25 +0800

    Update flash_attn_bhsd_v2

--- examples/flash_attention/flash_attn_bhsd_v2.py
+++ examples/flash_attention/flash_attn_bhsd_v2.py
@@ -17,8 +17,9 @@ def flash_attention_fwd(
     heads,
     dim,
 ):
-    block_M, block_N = 64, 64
-
+    block_M, block_N = 128, 128
+    NUM_STAGES = 2
+    L0_MAX_SIZE = 64 * 1024  # 64KB
     dtype = "float16"
     accum_dtype = "float"
 
@@ -28,15 +29,20 @@ def flash_attention_fwd(
 
     block_num = seq_len // block_M * heads * batch
 
+    n_num = T.max(T.ceildiv(block_N * dim * DataType(dtype).bits // 8, L0_MAX_SIZE), 1)
+    block_K = T.ceildiv(block_N, n_num)
+    block_D = T.ceildiv(dim, n_num)
+    num_stages = T.min(T.ceildiv(seq_len, block_N), NUM_STAGES).value
+
     @T.prim_func
     def main(
-            Q: T.Tensor(shape, dtype),  # type: ignore
-            K: T.Tensor(shape, dtype),  # type: ignore
-            V: T.Tensor(shape, dtype),  # type: ignore
+            Q: T.Tensor(shape, dtype),       # type: ignore
+            K: T.Tensor(shape, dtype),       # type: ignore
+            V: T.Tensor(shape, dtype),       # type: ignore
             Output: T.Tensor(shape, dtype),  # type: ignore
-            workspace_1: T.Tensor([block_num, block_M, block_N], accum_dtype),
-            workspace_2: T.Tensor([block_num, block_M, block_N], dtype),
-            workspace_3: T.Tensor([block_num, block_M, dim], accum_dtype),
+            workspace_1: T.Tensor([block_num, block_M, block_N], accum_dtype),  # type: ignore
+            workspace_2: T.Tensor([block_num, block_M, block_N], dtype),        # type: ignore
+            workspace_3: T.Tensor([block_num, block_M, dim], accum_dtype),      # type: ignore
     ):
         with T.Kernel(block_num, is_npu=True) as (cid, vid):
             bx = cid % (seq_len // block_M)
@@ -44,45 +50,43 @@ def flash_attention_fwd(
             bz = cid // (seq_len // block_M) // heads % batch
 
             q_l1 = T.alloc_L1([block_M, dim], dtype)
-            k_l1 = T.alloc_L1([block_N, dim], dtype)
-            v_l1 = T.alloc_L1([block_N, dim], dtype)
+            k_l1 = T.alloc_L1([block_K, dim], dtype)
+            v_l1 = T.alloc_L1([block_N, block_D], dtype)
 
             acc_s_l1 = T.alloc_L1([block_M, block_N], dtype)
 
-            acc_s_l0c = T.alloc_L0C([block_M, block_N], accum_dtype)
-            acc_o_l0c = T.alloc_L0C([block_M, dim], accum_dtype)
+            acc_s_l0c = T.alloc_L0C([block_M, block_K], accum_dtype)
+            acc_o_l0c = T.alloc_L0C([block_M, block_D], accum_dtype)
 
             acc_o = T.alloc_ub([block_M // 2, dim], accum_dtype)
-            sumexp = T.alloc_ub([block_M // 2], accum_dtype)
-            m_i = T.alloc_ub([block_M // 2], accum_dtype)
+            sumexp = T.alloc_ub([block_M // 2, 1], accum_dtype)
+            sumexp_2d = T.alloc_ub([block_M // 2, dim], accum_dtype)
+            m_i = T.alloc_ub([block_M // 2, 1], accum_dtype)
+            m_i_2d = T.alloc_ub([block_M // 2, block_N], accum_dtype)
 
             acc_s_ub = T.alloc_ub([block_M // 2, block_N], accum_dtype)
-            m_i_prev = T.alloc_ub([block_M // 2], accum_dtype)
+            m_i_prev = T.alloc_ub([block_M // 2, 1], accum_dtype)
+            m_i_prev_2d = T.alloc_ub([block_M // 2, dim], accum_dtype)
             acc_s_ub_ = T.alloc_ub([block_M // 2, block_N], accum_dtype)
-            tmp_ub = T.alloc_ub([3 * DataType(accum_dtype).bits // 8 * block_M // 2 * block_N], "uint8")
-            sumexp_i_ub = T.alloc_ub([block_M // 2], accum_dtype)
+            tmp_ub = T.alloc_ub([1 * DataType(accum_dtype).bits // 8 * block_M // 2 * block_N], "uint8")
+            sumexp_i_ub = T.alloc_ub([block_M // 2, 1], accum_dtype)
             acc_s_half = T.alloc_ub([block_M // 2, block_N], dtype)
             acc_o_ub = T.alloc_ub([block_M // 2, dim], accum_dtype)
             acc_o_half = T.alloc_ub([block_M // 2, dim], dtype)
 
-            # with T.Scope("C"):
-            T.copy(Q[bz, by, bx * block_M:(bx + 1) * block_M, :], q_l1)
-            for k in T.serial(T.ceildiv(seq_len, block_N)):
-                T.copy(K[bz, by, k * block_N:(k + 1) * block_N, :], k_l1)
-                T.gemm_v0(q_l1, k_l1, acc_s_l0c, transpose_B=True, init=True)
-                T.copy(acc_s_l0c, workspace_1[cid, :, :])
 
-                T.copy(workspace_2[cid, :, :], acc_s_l1)
-
-                T.copy(V[bz, by, k * block_N:(k + 1) * block_N, :], v_l1)
-                T.gemm_v0(acc_s_l1, v_l1, acc_o_l0c, init=True)
-                T.copy(acc_o_l0c, workspace_3[cid, :, :])
-
-            # with T.Scope("V"):
             T.tile.fill(acc_o, 0.0)
             T.tile.fill(sumexp, 0.0)
             T.tile.fill(m_i, -2**30)
-            for _k in T.serial(T.ceildiv(seq_len, block_N)):
+            T.copy(Q[bz, by, bx * block_M:(bx + 1) * block_M, :], q_l1)
+
+            for k in T.Pipelined(T.ceildiv(seq_len, block_N), num_stages=num_stages):
+
+                for n_i in T.serial(n_num):
+                  T.copy(K[bz, by, k * block_N + n_i * block_K : k * block_N + (n_i + 1) * block_K, :], k_l1)
+                  T.gemm_v0(q_l1, k_l1, acc_s_l0c, transpose_B=True, init=True)
+                  T.copy(acc_s_l0c, workspace_1[cid, :, n_i * block_K : (n_i + 1) * block_K])
+
                 T.tile.fill(acc_s_ub, 0.0)
                 T.copy(m_i, m_i_prev)
                 T.copy(workspace_1[cid, vid * block_M // 2:vid * block_M // 2 + block_M // 2, :], acc_s_ub_)
@@ -93,24 +97,34 @@ def flash_attention_fwd(
                 T.tile.sub(m_i_prev, m_i_prev, m_i)
                 T.tile.exp(m_i_prev, m_i_prev)
 
-                for h_i in range(block_M // 2):
-                    T.tile.sub(acc_s_ub[h_i, :], acc_s_ub[h_i, :], m_i[h_i])  # -
+                T.tile.broadcast(m_i_2d, m_i, tmp_ub)
+                T.tile.sub(acc_s_ub, acc_s_ub, m_i_2d)
 
                 T.tile.exp(acc_s_ub, acc_s_ub)
                 T.tile.reduce_sum(sumexp_i_ub, acc_s_ub, tmp_ub, dim=-1)
-                T.tile.mul(sumexp, sumexp, m_i_prev)  # check
+                T.tile.mul(sumexp, sumexp, m_i_prev)
                 T.tile.add(sumexp, sumexp, sumexp_i_ub)
-
-                for h_i in range(block_M // 2):
-                    T.tile.mul(acc_o[h_i, :], acc_o[h_i, :], m_i_prev[h_i])
 
                 T.copy(acc_s_ub, acc_s_half)
                 T.copy(acc_s_half, workspace_2[cid, vid * block_M // 2:vid * block_M // 2 + block_M // 2, :])
+
+                T.copy(workspace_2[cid, :, :], acc_s_l1)
+
+                for n_i in T.serial(n_num):
+                  T.copy(V[bz, by, k * block_N : (k + 1) * block_N, n_i * block_D : (n_i + 1) * block_D], v_l1)
+                  T.gemm_v0(acc_s_l1, v_l1, acc_o_l0c, init=True)
+                  T.copy(acc_o_l0c, workspace_3[cid, :, n_i * block_D : (n_i + 1) * block_D])
+
+
+                T.tile.broadcast(m_i_prev_2d, m_i_prev, tmp_ub)
+                T.tile.mul(acc_o, acc_o, m_i_prev_2d)
+
                 T.copy(workspace_3[cid, vid * block_M // 2:vid * block_M // 2 + block_M // 2, :], acc_o_ub)
                 T.tile.add(acc_o, acc_o, acc_o_ub)
 
-            for h_i in range(block_M // 2):
-                T.tile.div(acc_o[h_i, :], acc_o[h_i, :], sumexp[h_i])
+
+            T.tile.broadcast(sumexp_2d, sumexp, tmp_ub)
+            T.tile.div(acc_o, acc_o, sumexp_2d)
 
             T.copy(acc_o, acc_o_half)
             T.copy(acc_o_half, Output[bz, by, bx * block_M + vid * block_M // 2:bx * block_M + vid * block_M // 2 + block_M // 2, :])
@@ -122,8 +136,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--B", type=int, default=1, help="batch size")
     parser.add_argument("--S", type=int, default=128, help="seq len")
-    parser.add_argument("--H", type=int, default=1, help="attention head size")
-    parser.add_argument("--D", type=int, default=512, help="hidden dim")
+    parser.add_argument("--H", type=int, default=16, help="attention head size")
+    parser.add_argument("--D", type=int, default=128, help="hidden dim")
     args = parser.parse_args()
     B, S, H, D = args.B, args.S, args.H, args.D
 
