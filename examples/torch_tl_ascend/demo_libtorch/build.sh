3d39b3cb700916adf34a8e97740f54acf9ccf8b3
liggest <liggest@sina.cn>
2026-01-28 23:53:07 +0800

    revise demo_libtorch

--- examples/torch_tl_ascend/demo_libtorch/build.sh
+++ examples/torch_tl_ascend/demo_libtorch/build.sh
@@ -1,5 +1,14 @@
-mkdir build && cd build
-CMAKE_PREFIX_TORCH=`python3 -c 'import torch;print(torch.utils.cmake_prefix_path)'`
-CMAKE_PREFIX_TORCH_NPU="path/to/libtorch_npu"
-cmake -DCMAKE_PREFIX_PATH="${CMAKE_PREFIX_TORCH};${CMAKE_PREFIX_TORCH_NPU}" ..
+#!/bin/bash
+set -x
+
+CMAKE_PREFIX_TORCH=$(python3 -c 'import torch;print(torch.utils.cmake_prefix_path)')
+CMAKE_PREFIX_TORCH_NPU="/mnt/workspace/torch_npu/libtorch_npu"  # 编译好的 libtorch_npu 的根目录
+TORCH_NPU_SOURCE_DIR="${TORCH_NPU_SOURCE_DIR:-/mnt/workspace/torch_npu}"  # torch_npu 源码目录（third_party 目录所在的目录），此处值为 /mnt/workspace/torch_npu
+
+mkdir -p build && cd build
+
+cmake -DCMAKE_PREFIX_PATH="${CMAKE_PREFIX_TORCH};${CMAKE_PREFIX_TORCH_NPU}" \
+      -DTORCH_NPU_SOURCE_DIR="${TORCH_NPU_SOURCE_DIR}" \
+      ..
+
 cmake --build . --config Release
