fccf2ca145a01dc8e8862ab9749580f46a4026d0
Yunpeng Jia <jiayunpeng4@huawei.com>
2026-01-29 17:42:13 +0800

    [AscendNPU-IR][Developer] Support slice Operation

--- src/target/codegen_npuir_dev.cc
+++ src/target/codegen_npuir_dev.cc
@@ -2149,7 +2149,20 @@ void CodeGenTileLangNPUIRDEV::CreateHIVMBinaryVectorOp(const CallNode *op) {
         src = VisitExpr_(buffer_node);
       } else {
         src = GenExtractSliceFromRegion(region_node);
-        buffer_shape = region_extent;
+
+        auto tensorType = src.getType().dyn_cast<mlir::TensorType>();
+        
+        buffer_shape.clear();
+
+        for (int64_t dim : tensorType.getShape()) {
+          if (dim >= 0) {
+            buffer_shape.push_back(
+                tir::make_const(DataType::Int(64), dim));
+          } else {
+            ICHECK(false)
+                << "dynamic tensor shape not supported yet";
+          }
+        }
       }
     }
   };
@@ -2160,6 +2173,11 @@ void CodeGenTileLangNPUIRDEV::CreateHIVMBinaryVectorOp(const CallNode *op) {
   processImm(src1, 1, buffer_shape1);
   // dst
   const CallNode *region_node_dst = op->args[2].as<CallNode>();
+<<<<<<< HEAD
+=======
+  // Result will always be a vector. No need to add scalar check.
+  // mlir::Value dst = GenExtractSliceFromRegion(region_node_dst);
+>>>>>>> 3335e76 ([AscendNPU-IR][Developer] Support slice Operation)
 
   tvm::tl::RegionOp region_dst_tmp(region_node_dst->args, vmap);
   Array<Range> dst_range = region_dst_tmp.GetRanges();
@@ -2177,7 +2195,13 @@ void CodeGenTileLangNPUIRDEV::CreateHIVMBinaryVectorOp(const CallNode *op) {
   llvm::SmallVector<int64_t> dims =
       getBroadcastDim(buffer_shape0, buffer_shape1);
   mlir::DenseI64ArrayAttr broadcast = builder.getDenseI64ArrayAttr(dims);
+<<<<<<< HEAD
 
+=======
+  // typerange
+  // mlir::Type dst_type = dst.getType();
+  // mlir::TypeRange result_tensors(&dst_type, 1);
+>>>>>>> 3335e76 ([AscendNPU-IR][Developer] Support slice Operation)
   // Create hivm::op
   auto loc = builder.getUnknownLoc();
   mlir::Value newOpValue;
@@ -2206,6 +2230,8 @@ void CodeGenTileLangNPUIRDEV::CreateHIVMBinaryVectorOp(const CallNode *op) {
     auto newOp = builder.create<T>(loc, insertBase.getType(), mlir::ValueRange{src0, src1},
         mlir::ValueRange{insertBase}, transpose, broadcast);
     newOpValue = newOp->getResult(0);
+    std::cout<<"newOp: \n";
+    newOp.dump();
   }
 
   mlir::Value result = needInsertSlice
