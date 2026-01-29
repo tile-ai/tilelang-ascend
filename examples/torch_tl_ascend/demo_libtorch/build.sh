cdd018a5c3d4006af01120a7140a992c79d71bf9
liggest <liggest@sina.cn>
2026-01-29 22:45:14 +0800

    Use English comment

--- examples/torch_tl_ascend/demo_libtorch/build.sh
+++ examples/torch_tl_ascend/demo_libtorch/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -x
 
-python prepare.py || { echo "Failed to prepare libop.so"; exit 1; }  # 编译 tilelang 算子，获取 libop.so
+python prepare.py || { echo "Failed to prepare libop.so"; exit 1; }  # compile tilelang operator and get libop.so
 
 CMAKE_PREFIX_TORCH=$(python -c 'import torch;print(torch.utils.cmake_prefix_path)')
-CMAKE_PREFIX_TORCH_NPU="/mnt/workspace/torch_npu/libtorch_npu"  # 编译好的 libtorch_npu 的根目录
-TORCH_NPU_SOURCE_DIR="${TORCH_NPU_SOURCE_DIR:-/mnt/workspace/torch_npu}"  # torch_npu 源码目录（third_party 目录所在的目录），此处值为 /mnt/workspace/torch_npu
+CMAKE_PREFIX_TORCH_NPU="/mnt/workspace/torch_npu/libtorch_npu"  # root path to compiled libtorch_npu
+TORCH_NPU_SOURCE_DIR="${TORCH_NPU_SOURCE_DIR:-/mnt/workspace/torch_npu}"  # source path to torch_npu (where "third_party" locates), default /mnt/workspace/torch_npu
 
 mkdir -p build && cd build
 
