8e5cf83a48f8cb729ba6f57fcde726cfc6c96392
z00520135 <zhangwei503@huawei.com>
2026-01-31 15:39:38 +0800

    ub分配pass逻辑提交

--- src/transform/ascend_vid_reduction.cc
+++ src/transform/ascend_vid_reduction.cc
@@ -33,12 +33,120 @@ public:
   static PrimFunc Substitute(PrimFunc f, PassContext ctx) {
     arith::Analyzer analyzer;
     AscendVidReduction substituter(&analyzer);
+    PrimFuncNode *fptr = f.CopyOnWrite();
+    // // TODO:访问buffer是否这样实现？
+    // tir::PostOrderVisit(f->body, [&](const ObjectRef& obj) {
+    //     if (const auto* realize = obj.as<tir::BlockRealizeNode>()) {
+    //         for (auto buf : realize->block->alloc_buffers) {
+    //             ModifyBufferShape(buf);
+    //         }
+    //     }
+    // });
+    // // TODO:影响及作用？
+    bool ascend_vid_reduction = ctx->GetConfig<Bool>(ascendVidReduction, Bool(false)).value();
+    if (!ascend_vid_reduction) {
+      return f;
+    }
+    fptr->body = substituter.VisitStmt(f->body);
     return f;
   }
 
 private:
   using arith::IRMutatorWithAnalyzer::IRMutatorWithAnalyzer;
+
+  std::unordered_map<Buffer, Buffer, ObjectPtrHash, ObjectPtrEqual> buffer_map_;
+
+  bool IsUbBuffer(const Buffer& buffer) const {
+    // TODO:buffer不是指指针为啥->data取内容
+    if (buffer->data->type_annotation.defined()) {
+      if (const auto* ptr_type = buffer->data->type_annotation.as<PointerTypeNode>()) {
+        return ptr_type->storage_scope == "shared";
+      }
+    }
+    return false;
+  }
+
+  Buffer ModifyBufferShape(const Buffer& buffer) {
+    if (buffer->shape.empty()) {
+      return buffer;
+    }
+    ObjectPtr<BufferNode> new_buffer = make_object<BufferNode>(*buffer.get());
+
+    new_buffer->shape = ModifyExtents(buffer->shape);
+
+    new_buffer->strides.clear();
+    new_buffer->elem_offset = PrimExpr();
+
+    return Buffer(new_buffer);
+
+  }
+
+  Array<PrimExpr> ModifyExtents(const Array<PrimExpr>& extents) {
+    if (extents.empty()) {
+      return extents;
+    }
+    Array<PrimExpr> new_extents;
+    arith::Analyzer analyzer;
+
+    for (size_t i=0; i < extents.size(); i++) {
+      if (i == 0) {
+        PrimExpr first_extent = analyzer.Simplify(extents[i]);
+        if (const auto* int_imm = first_extent.as<IntImmNode>()) {
+          int64_t new_value = int_imm->value / 2;
+          if (new_value < 1) {
+            new_value = 1;
+            new_extents.push_back(IntImm(first_extent.dtype(), new_value));
+          }
+          // TODO:非小于1不需要处理？
+        } else if (const auto* cast_node = first_extent.as<CastNode>()) {
+          if (const auto* int_imm = cast_node->value.as<IntImmNode>()) {
+            int64_t new_value = int_imm->value / 2;
+            if (new_value < 1) {
+              new_value = 1;
+              new_extents.push_back(IntImm(cast_node->dtype, new_value));
+            } else {
+              new_extents.push_back(floordiv(first_extent, 2));
+            }
+          }
+        } else {
+          new_extents.push_back(floordiv(first_extent, 2));
+        }
+      } else {
+        new_extents.push_back(extents[i]);
+      }
+    }
+    return new_extents;
+  }
+
+  Stmt VisitStmt_(const BlockNode* op) override {
+    if (op->alloc_buffers.defined()) {
+      for (const auto& buffer : op->alloc_buffers) {
+        if (IsUbBuffer(buffer)) {
+          Buffer new_buffer = ModifyBufferShape(buffer);
+          buffer_map_[buffer] = new_buffer;
+        }
+      }
+    }
+    Stmt new_body = this->VisitStmt(op->body);
+
+    Array<Buffer> new_alloc_buffers;
+    if (op->alloc_buffers.defined()) {
+      for (const auto& buffer : op->alloc_buffers) {
+        auto it = buffer_map_.find(buffer);
+        if (it != buffer_map_.end()) {
+          new_alloc_buffers.push_back(it->second);
+        } else {
+          new_alloc_buffers.push_back(buffer);
+        }
+      }
+    }
+    ObjectPtr<BlockNode> new_block = make_object<BlockNode>(*op);
+    new_block->body = new_body;
+    new_block->alloc_buffers = new_alloc_buffers;
+    return Stmt(new_block);
+  }
 };
+
 
 
 tvm::transform::Pass AscendVidReduction() {
