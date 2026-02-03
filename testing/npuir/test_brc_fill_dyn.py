0492b59e2b0c36d14c5dcc17d646e588749ff5fb
l00913017 <lizhipeng97@huawei.com>
2026-02-03 20:14:05 +0800

    [AscendNPU-IR][Expert&Develop] npuir_brc supporting variable parameters.

--- testing/npuir/test_brc_fill_dyn.py
+++ testing/npuir/test_brc_fill_dyn.py
@@ -14,8 +14,8 @@ parser = argparse.ArgumentParser(description="NPU Kernel Compilation")
 parser.add_argument("--M", type=int, default=512, help="")
 parser.add_argument("--N", type=int, default=512, help="")
 parser.add_argument("--K", type=int, default=512, help="")
-parser.add_argument("--block_M", type=int, default=64, help="")
-parser.add_argument("--block_N", type=int, default=64, help="")
+parser.add_argument("--block_M", type=int, default=32, help="")
+parser.add_argument("--block_N", type=int, default=32, help="")
 
 
 def vec_brc_dev(M, N, K, block_M, block_N):
@@ -27,12 +27,15 @@ def vec_brc_dev(M, N, K, block_M, block_N):
     @T.prim_func
     def main(
             A: T.Tensor((M, K), dtype),
-            B: T.Tensor((1, block_N), dtype)
+            B: T.Tensor((1, block_N), dtype),
+            C: T.Tensor((M, block_N), dtype)
     ):
         with T.Kernel(BLOCK_SIZE, is_npu=True) as (cid, _):
             A_VEC = T.alloc_shared((block_M, block_N), dtype)
             B_VEC = T.alloc_shared((1, block_N), dtype)
+            C_VEC = T.alloc_shared((M, block_N), dtype)
             T.copy(B,B_VEC)
+            T.copy(C,C_VEC)
             for i in T.serial(T.ceildiv(m_num*n_num, BLOCK_SIZE)):
                 block_id = i * BLOCK_SIZE + cid
                 if block_id < m_num * n_num:
@@ -40,7 +43,11 @@ def vec_brc_dev(M, N, K, block_M, block_N):
                     block_id_n = block_id % n_num
                     bx = block_id_m * block_M
                     by = block_id_n * block_N
-                    T.npuir_fill(A_VEC, B_VEC)
+                    # T.npuir_brc(C_VEC[0,0], A_VEC)
+                    # T.npuir_brc(bx+1, A_VEC)
+                    # T.npuir_brc(i+1, A_VEC)
+                    # T.npuir_brc(B_VEC, A_VEC)
+                    T.npuir_brc(C_VEC[0:1,:], A_VEC)
                     T.copy(A_VEC, A[bx, by])
 
     return main
@@ -54,12 +61,15 @@ def vec_brc_exp(M, N, K, block_M, block_N):
     @T.prim_func
     def main(
             A: T.Tensor((M, K), dtype),
-            B: T.Tensor((1, block_N), dtype)
+            B: T.Tensor((1, block_N), dtype),
+            C: T.Tensor((M, block_N), dtype)
     ):
         with T.Kernel(BLOCK_SIZE, is_npu=True) as (cid, _):
             A_VEC = T.alloc_ub((block_M, block_N), dtype)
             B_VEC = T.alloc_ub((1, block_N), dtype)
+            C_VEC = T.alloc_ub((M, block_N), dtype)
             T.copy(B,B_VEC)
+            T.copy(C,C_VEC)
             for i in T.serial(T.ceildiv(m_num*n_num, BLOCK_SIZE)):
                 block_id = i * BLOCK_SIZE + cid
                 if block_id < m_num * n_num:
@@ -67,7 +77,11 @@ def vec_brc_exp(M, N, K, block_M, block_N):
                     block_id_n = block_id % n_num
                     bx = block_id_m * block_M
                     by = block_id_n * block_N
-                    T.npuir_brc(i, A_VEC)
+                    # T.npuir_brc(C_VEC[0,0], A_VEC)
+                    # T.npuir_brc(bx+1, A_VEC)
+                    # T.npuir_brc(i+1, A_VEC)
+                    # T.npuir_brc(B_VEC, A_VEC)
+                    T.npuir_brc(C_VEC[0:1,:], A_VEC)
                     T.copy(A_VEC, A[bx, by])
 
     return main
@@ -113,10 +127,15 @@ def run_test(main_args):
 
     a = generate_tensor(shape, dtype).npu()
     b = generate_tensor((1, main_args.block_N), dtype).npu()
+    c = generate_tensor((main_args.M, main_args.block_N), dtype).npu()
 
-    compiled_kernel(a,b)
-    print("Actual Result:")
+    compiled_kernel(a,b,c)
+    print("====a====")
     print(a)
+    print("====b====")
+    print(b)
+    print("====c====")
+    print(c)
 
 if __name__ == "__main__":
     args = parser.parse_args()
