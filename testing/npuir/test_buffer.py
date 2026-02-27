3f8e7c114dd0000675e19e08fbd20ece0639ce43
XinYang <88878364+CeleNewYear@users.noreply.github.com>
2026-02-27 12:00:39 +0800

    Update test_buffer.py

--- testing/npuir/test_buffer.py
+++ testing/npuir/test_buffer.py
@@ -5,7 +5,7 @@ import tilelang
 import tilelang.language as T
 
 
-@tilelang.jit(out_idx=[-1], target="npuir")
+@tilelang.jit(target="npuir")
 def matmul(M, N, K, block_M, block_N, block_K, dtype="float16", accum_dtype="float32"):
     @T.prim_func
     def gemm(
