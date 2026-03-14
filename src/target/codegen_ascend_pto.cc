881dde46e1254812d5e4c527a17e25e6e9503ff6
HanXu <46563594+ArmandAlbert@users.noreply.github.com>
2026-03-14 17:11:15 +0800

    [bugfix] Fix pto copy_ub_to_gm bug caused by interface templates/params and pre-compilation. (#604)

--- src/target/codegen_ascend_pto.cc
+++ src/target/codegen_ascend_pto.cc
@@ -1104,8 +1104,7 @@ void CodeGenTileLangAscendPto::CallExternCodegen(const CallNode *op) {
           this->stream << ")";
         }
         if (op_name.find("copy_ub_to_gm") != std::string::npos) {
-          int32_t type_len = GetTypeLen(global_tensor_template[String(tensor_addr)].dtype);
-          this->stream << ", " << ub_valid_shapes[3] << ", " << src_offset << "," << type_len << ");\n";
+          this->stream << ", " << ub_valid_shapes[3] << ", " << src_offset << ");\n";
         } else {
           this->stream << ", " << src_var_id << ");\n";
         }
