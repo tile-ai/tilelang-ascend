90e9e428d3f89b2040a066ccd8c9fb6a601fa8e3
d00957057 <18810038939@163.com>
2026-03-26 16:38:42 +0800

    CV memref EnableMultiBuffer

--- tilelang/engine/lower.py
+++ tilelang/engine/lower.py
@@ -284,6 +284,7 @@ def lower(
         pipeline.add(transforms.tilelangir.insert_workspace)
         pipeline.add(transforms.tilelangir.cv_split)
         pipeline.add(transforms.tilelangir.infer_mem_scope)
+        pipeline.add(transforms.tilelangir.enable_multi_buffer)
         if dump_ir:
             pipeline.enable_ir_printing()
         mlir_str = pipeline.run(mlir_str)
