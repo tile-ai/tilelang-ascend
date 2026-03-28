5c3ef50a7b823c1e727e1e2248a4a91432251b4c
hanxu88 <hanxu88@huawei.com>
2026-03-28 15:01:53 +0800

    [Add] Add logic to automatically handle tmp buffer allocation.

--- src/transform/ascend_memory_planning.cc
+++ src/transform/ascend_memory_planning.cc
@@ -62,7 +62,14 @@ public:
           fn_attr->dict.at("address_map").as<Map<Var, PrimExpr>>().value();
     }
 
-    AscendMemoryPlanner planner(f, external_address_map,
+    Map<Var, Array<PrimExpr>> external_shape_map;
+    if (fn_attr->dict.count("buffer_shapess")) {
+      external_shape_map = fn_attr->dict.at("buffer_shapess")
+                               .as<Map<Var, Array<PrimExpr>>>()
+                               .value();
+    }
+
+    AscendMemoryPlanner planner(f, external_address_map, external_shape_map,
                                 auto_ascend_memory_planning);
     auto address_map = planner.GetAddressMap();
 
@@ -80,6 +87,7 @@ private:
   public:
     explicit AscendMemoryPlanner(const PrimFunc &func,
                                  Map<Var, PrimExpr> external_address_map,
+                                 Map<Var, Array<PrimExpr>> external_shape_map,
                                  bool auto_plan = false) {
       memory_auto_plan = auto_plan;
       memory_limits_ = {{"shared.dyn", ASCEND_SHARED_DYN_MEM_SIZE},
@@ -89,6 +97,7 @@ private:
                         {"shared", ASCEND_SHARED_MEM_SIZE}};
 
       SetPreAllocBuffer(external_address_map);
+      SetTmpBuffers(external_shape_map);
 
       operator()(func->body);
       PlanMemory();
@@ -284,6 +293,16 @@ private:
                      << " already been allocated.";
         }
         pre_alloc_buffer_[buf->name_hint] = addr_offset;
+      }
+    }
+
+    void SetTmpBuffers(Map<Var, Array<PrimExpr>> external_shape_map) {
+      for (auto &kv : external_shape_map) {
+        const VarNode *buf = kv.first.get();
+        std::string scope = GetPtrStorageScope(kv.first);
+        if (!pre_alloc_buffer_.count(buf->name_hint) && scope == "shared") {
+          tmp_buffers.push_back(buf);
+        }
       }
     }
 
@@ -508,11 +527,16 @@ private:
                                   const std::vector<const VarNode *> &buffers) {
       bool check_overflow = false; // reserve memory overflow check
       int64_t current_offset = 0;
+      int64_t max_offset = 0;
       for (const VarNode *buffer : origin_buffer) {
         if (std::find(buffers.begin(), buffers.end(), buffer) !=
             buffers.end()) {
           if (pre_alloc_buffer_.count(buffer->name_hint)) {
             address_map_[buffer] = pre_alloc_buffer_[buffer->name_hint];
+            max_offset = std::max(
+                max_offset,
+                static_cast<int64_t>(pre_alloc_buffer_[buffer->name_hint] +
+                                     buffer_sizes_[buffer]));
           } else {
             int64_t buf_size = buffer_sizes_[buffer];
             if (current_offset + buf_size > memory_limits_[scope] &&
@@ -527,7 +551,26 @@ private:
               address_map_[buffer] = current_offset;
               current_offset =
                   static_cast<int64_t>(AlignUp(current_offset + buf_size, 32));
+              max_offset = std::max(max_offset, current_offset);
             }
+          }
+        }
+      }
+      // allocate tmp buffer in shared memory after origin buffer
+      if (scope == "shared") {
+        for (auto buffer : tmp_buffers) {
+          int64_t buf_size = buffer_sizes_[buffer];
+          if (max_offset + buf_size > memory_limits_[scope] && check_overflow) {
+            LOG(FATAL) << "Linear tmp memory allocation failed! Out of memory "
+                          "in scope: "
+                       << scope << "\nBuffer: " << buffer->name_hint
+                       << "\nRequired size: " << buffer_sizes_[buffer]
+                       << "\nCurrent offset: " << max_offset
+                       << "\nMemory limit: " << memory_limits_[scope];
+          } else {
+            address_map_[buffer] = max_offset;
+            max_offset =
+                static_cast<int64_t>(AlignUp(max_offset + buf_size, 32));
           }
         }
       }
@@ -858,6 +901,8 @@ private:
     std::vector<const VarNode *> origin_buffer; // original buffer list
     std::unordered_set<std::string>
         buffer_names_; // buffer names for duplicate check
+    std::vector<const VarNode *>
+        tmp_buffers; // temp buffer list for memory planning
 
     std::multimap<uint64_t, StorageEntry *> const_free_map_;
     std::list<StorageEntry *> sym_free_list_;
