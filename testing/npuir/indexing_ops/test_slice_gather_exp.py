ef2ebfd9b448a1babd4909008d6095864f0452b8
l00913017 <lizhipeng97@huawei.com>
2026-04-03 09:49:07 +0800

    [AscendNPU-IR] Support the gather operator in Dev mode, and add test cases for the gather and not operators.

--- testing/npuir/indexing_ops/test_slice_gather_exp.py
+++ testing/npuir/indexing_ops/test_slice_gather_exp.py
@@ -1,6 +1,5 @@
 import pytest
 import torch
-import torch_npu  # noqa: F401
 
 import tilelang
 import tilelang.language as T
@@ -13,13 +12,16 @@ pytestmark = [
     pytest.mark.mode("Expert"),
 ]
 
-GATHER_CASES = [(256, 256, 32, 32, 1)]
+GATHER_CASES = [(260, 260, 32, 32, 1)]
 DTYPES = ["float16"]
+
+torch.npu.set_device(11)
 
 
 def vec_gather(M, N, block_M, block_N, dim, dtype="float16"):
     itype = "int32"
-    n_num = N // block_N
+    m_num = T.ceildiv(M, block_M)
+    n_num = T.ceildiv(N, block_N)
 
     @T.prim_func
     def sliceGatherKernel(
@@ -27,7 +29,7 @@ def vec_gather(M, N, block_M, block_N, dim, dtype="float16"):
         B: T.Tensor((M, N), itype),
         C: T.Tensor((M, N), dtype),
     ):
-        with T.Kernel(T.ceildiv(M, block_M) * T.ceildiv(N, block_N), is_npu=True) as (
+        with T.Kernel(m_num * n_num, is_npu=True) as (
             cid,
             _,
         ):
@@ -38,10 +40,19 @@ def vec_gather(M, N, block_M, block_N, dim, dtype="float16"):
             A_VEC = T.alloc_ub([block_M, block_N], dtype)
             B_VEC = T.alloc_ub([block_M, block_N], itype)
             C_VEC = T.alloc_ub([block_M, block_N], dtype)
-            T.copy(A[bx : bx + block_M, by : by + block_N], A_VEC)
-            T.copy(B[bx : bx + block_M, by : by + block_N], B_VEC)
-            T.gather(A_VEC, C_VEC, B_VEC)
-            T.copy(C_VEC, C[bx : bx + block_M, by : by + block_N])
+
+            valid_m = T.min(block_M, M - bx)
+            valid_n = T.min(block_N, N - by)
+
+            T.copy(A[bx : bx + valid_m, by : by + valid_n], A_VEC)
+            T.copy(B[bx : bx + valid_m, by : by + valid_n], B_VEC)
+            T.clear(C_VEC)
+            T.gather(
+                A_VEC[:valid_m, :valid_n],
+                C_VEC[:valid_m, :valid_n],
+                B_VEC[:valid_m, :valid_n],
+            )
+            T.copy(C_VEC[:valid_m, :valid_n], C[bx : bx + valid_m, by : by + valid_n])
 
     return sliceGatherKernel
 
@@ -53,18 +64,36 @@ def test_vec_gather(dtype, M, N, block_M, block_N, dim):
     kernel = tilelang.compile(compile_kernel, target="npuir")
 
     a = gen_tensor((M, N), dtype, kind="randn")
-    b = gen_tensor((M, N), "int32", kind="randint", low=0, high=block_N)
+
+    b = torch.empty((M, N), dtype=torch.int32, device=a.device)
+    for bx in range(0, M, block_M):
+        for by in range(0, N, block_N):
+            valid_m = min(block_M, M - bx)
+            valid_n = min(block_N, N - by)
+            b[bx : bx + valid_m, by : by + valid_n] = torch.randint(
+                low=0,
+                high=valid_n,
+                size=(valid_m, valid_n),
+                dtype=torch.int32,
+                device=a.device,
+            )
+
     c = gen_tensor((M, N), dtype, kind="zeros")
 
     ref_c = torch.zeros_like(a)
 
     for bx in range(0, M, block_M):
         for by in range(0, N, block_N):
-            a_tile = a[bx : bx + block_M, by : by + block_N]
-            b_tile = b[bx : bx + block_M, by : by + block_N]
-            ref_c[bx : bx + block_M, by : by + block_N] = torch.gather(
+            valid_m = min(block_M, M - bx)
+            valid_n = min(block_N, N - by)
+
+            a_tile = a[bx : bx + valid_m, by : by + valid_n]
+            b_tile = b[bx : bx + valid_m, by : by + valid_n]
+
+            ref_c[bx : bx + valid_m, by : by + valid_n] = torch.gather(
                 a_tile, dim=1, index=b_tile
             )
+
     kernel(a, b, c)
 
     assert_close(c.cpu(), ref_c.cpu(), dtype=dtype, rtol=1e-2, atol=1e-2)
