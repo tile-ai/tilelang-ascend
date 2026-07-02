fa91d9c3e80c5cb26560d789933709c1fd8102c8
liggest <liggest@sina.cn>
2026-07-02 22:49:29 +0800

    [CI] Comfort clang-format

--- src/op/ascend.cc
+++ src/op/ascend.cc
@@ -1388,7 +1388,7 @@ TIR_DEFINE_TL_BUILTIN(ascend_row_expand_sub_experiment)
 TIR_DEFINE_TL_BUILTIN(ascend_row_expand_div_experiment)
     .set_num_inputs(-1)
     .set_attr<TCallEffectKind>("TCallEffectKind",
-                            Integer(CallEffectKind::kOpaque));
+                               Integer(CallEffectKind::kOpaque));
 TIR_DEFINE_TL_BUILTIN(ascend_copy_cv_experiment)
     .set_num_inputs(3)
     .set_attr<TCallEffectKind>("TCallEffectKind",
