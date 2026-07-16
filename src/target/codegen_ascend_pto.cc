2b9be34fac1b024ed4d5f63ef7eee66688d7a7fc
caojian5 <caojian5@huawei.com>
2026-07-16 16:52:25 +0800

    [Fix] PTO CodeGen: fix BufferStoreNode eval order causing invalid C++ from if_then_else

--- src/target/codegen_ascend_pto.cc
+++ src/target/codegen_ascend_pto.cc
@@ -783,18 +783,23 @@ void CodeGenTileLangAscendPto::VisitExpr_(const BufferLoadNode *op,
 
 void CodeGenTileLangAscendPto::VisitStmt_(const BufferStoreNode *op) {
   auto var_name = var_idmap_[op->buffer->data.get()];
-  this->PrintIndent();
   std::string scope = op->buffer.scope();
-
   if (scope == "" || scope == "global") {
-    this->stream << "*(" << var_name << "_handle + "
-                 << PrintExpr(op->indices.back())
-                 << ") = " << PrintExpr(op->value) << ";\n";
+    std::string index = PrintExpr(op->indices.back());
+    std::string value = PrintExpr(op->value);
+    this->PrintIndent();
+    this->stream << "*(" << var_name << "_handle + " << index << ") = " << value
+                 << ";\n";
   } else if (scope == "local.var") {
-    this->stream << var_name << " = " << PrintExpr(op->value) << ";\n";
+    std::string value = PrintExpr(op->value);
+    this->PrintIndent();
+    this->stream << var_name << " = " << value << ";\n";
   } else {
-    this->stream << var_name << ".SetValue(" << PrintExpr(op->indices.back())
-                 << ", " << PrintExpr(op->value) << ");\n";
+    std::string index = PrintExpr(op->indices.back());
+    std::string value = PrintExpr(op->value);
+    this->PrintIndent();
+    this->stream << var_name << ".SetValue(" << index << ", " << value
+                 << ");\n";
   }
 }
 
